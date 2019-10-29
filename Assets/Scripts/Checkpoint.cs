39179936a5d14801ce4b0538de55b3251de7c014
Arttu <artturunen97@gmail.com>
2019-10-29 22:45:28 +0200

    Added checkpoints and Player braking when collide on hazard

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public Rigidbody2D rig;
+    public GameObject player;
+    public SpriteRenderer rendon;
+    public SpriteRenderer rendoff;
+    public bool isActive = false;
+    bool lock1 = true;
+
+    private player_collider script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+
+        script = player.GetComponentInChildren<player_collider>();
+        rig = player.GetComponent<Rigidbody2D>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (script.cp_pos == this.transform.position)
+        {
+            isActive = true;
+        }
+        else {
+
+            isActive = false;
+        
+        }
+
+        rendon.enabled = isActive;
+        rendoff.enabled = !(isActive);
+
+        if (!player.active && lock1 && isActive)
+        {
+            lock1 = false;
+            StartCoroutine(waiter());
+        }
+    }
+
+    IEnumerator waiter()
+    {
+
+        yield return new WaitForSeconds(2);
+        player.transform.position = script.cp_pos + new Vector3(0f,0.72f,0f);
+        
+        rig.constraints = RigidbodyConstraints2D.FreezeAll;
+        player.transform.eulerAngles = new Vector3(0,0,0);
+        rig.velocity = new Vector2(0,0);
+
+
+        player.SetActive(true);
+        lock1 = true;
+    }
+
+
+
+
+
+}
---
+++ Assets/Scripts/player_collider.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_collider : MonoBehaviour
+{
+
+    public Vector3 cp_pos;
+    public GameObject player;
+    public GameObject broken;
+    public Component[] parts;
+    public SpriteRenderer rend;
+    public PolygonCollider2D polcol;
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.collider.tag == "cp") {
+            cp_pos = col.collider.transform.position;
+            }
+
+        if (col.collider.tag == "bad") {
+
+            
+
+            GameObject obj = (GameObject)Instantiate(broken, transform.position, transform.rotation);
+            parts = obj.GetComponentsInChildren<Rigidbody2D>();
+
+            foreach (Rigidbody2D rig in parts) {
+                
+                float x = Random.Range(-1.0f, 1.0f);
+                float y = Random.Range(-1.0f, 1.0f);
+                rig.velocity = new Vector2(x,y)*5;
+                rig.angularVelocity = Random.Range(-1.0f, 1.0f) * 5;
+            }
+
+            player.SetActive(false);
+            //rend.enabled = false;
+            //Destroy(player);
+            //Instantiate(player, new Vector3(-0.4f, 2.2f, 7.3f), transform.rotation);
+
+        }
+
+    }
+
+
+}
