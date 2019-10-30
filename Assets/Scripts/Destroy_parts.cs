2d77cce41cf8204d7b6fca3d48dd9255e5597f98
Arttu Turunen <arttu.turunen@aalto.fi>
2019-10-30 20:32:14 +0200

    Ship pieces now disappear after respawning. Added also some bounciness to ship

---
+++ Assets/Scripts/Destroy_parts.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy_parts : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(waiter());
+    }
+
+    IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(this.gameObject);
+    }
+
+}
