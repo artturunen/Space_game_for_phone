563c20136e517b2cb816a029bbffbe0dfe0aaf38
Arttu Turunen <arttu.turunen@aalto.fi>
2019-10-31 20:46:56 +0200

    Added healt and healtbar. Impoved collision detection on player and added new textures for controls

---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Text txt;
+    private player_collider script;
+    public GameObject player;
+
+    private void Awake()
+    {
+        script = player.GetComponentInChildren<player_collider>();
+    }
+
+    void Start()
+    {
+        txt.text = script.hp.ToString("F0");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = script.hp.ToString("F0");
+    }
+}
--- Assets/Scripts/player_collider.cs
+++ Assets/Scripts/player_collider.cs
@@ -11,9 +11,47 @@ public class player_collider : MonoBehaviour
     public Component[] parts;
     public SpriteRenderer rend;
     public PolygonCollider2D polcol;
+    public Rigidbody2D rb;
+
+    public float acc = 0;
+    public float last_vel = 0;
+    public float hp = 100f;
+
+    private void Update()
+    {
+
+        if (hp <= 0) {
+            Kill_Player();
+        }
+
+    }
+
+    private void Awake()
+    {
+        rb = player.GetComponent<Rigidbody2D>();
+    }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
+
+        float force = GetImpactForce(col); 
+
+        if ( force >= 100f) {
+
+            hp -= force * 0.1f;
+        }
+        
+        //print(col.impulse.magnitude);
+
+        /*
+        if (rb.velocity.magnitude > 1) {
+
+            print("sattui " + rb.velocity.magnitude);
+            hp -= rb.velocity.magnitude*4;
+           
+        }
+        */
+
 
         if (col.collider.tag == "cp") {
             cp_pos = col.collider.transform.position;
@@ -21,27 +59,47 @@ public class player_collider : MonoBehaviour
 
         if (col.collider.tag == "bad") {
 
-            
-
-            GameObject obj = (GameObject)Instantiate(broken, transform.position, transform.rotation);
-            parts = obj.GetComponentsInChildren<Rigidbody2D>();
-
-            foreach (Rigidbody2D rig in parts) {
-                
-                float x = Random.Range(-1.0f, 1.0f);
-                float y = Random.Range(-1.0f, 1.0f);
-                rig.velocity = new Vector2(x,y)*5;
-                rig.angularVelocity = Random.Range(-1.0f, 1.0f) * 5;
-            }
-
-            player.SetActive(false);
-            //rend.enabled = false;
-            //Destroy(player);
-            //Instantiate(player, new Vector3(-0.4f, 2.2f, 7.3f), transform.rotation);
-
+            Kill_Player();
         }
 
     }
 
+    public void Kill_Player() {
+
+        if (player.active) { 
+
+            GameObject obj = (GameObject)Instantiate(broken, transform.position, transform.rotation);
+            parts = obj.GetComponentsInChildren<Rigidbody2D>();
+
+            foreach (Rigidbody2D rig in parts)
+            {
+
+                float x = Random.Range(-1.0f, 1.0f);
+                float y = Random.Range(-1.0f, 1.0f);
+                rig.velocity = new Vector2(x, y) * 5;
+                rig.angularVelocity = Random.Range(-1.0f, 1.0f) * 5;
+            }
+
+            hp = 100f;
+            player.SetActive(false);
+        }
+    }
+
+
+    public float GetImpactForce(Collision2D collision)
+    {
+        float impulse = 0F;
+
+        foreach (ContactPoint2D point in collision.contacts)
+        {
+            impulse += point.normalImpulse;
+        }
+
+        return impulse / Time.fixedDeltaTime;
+    }
+
 
 }
+
+
+
