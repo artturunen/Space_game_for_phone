da80cd48ad56eedd1ab1daee02b62313cf116d95
Arttu Turunen <arttu.turunen@aalto.fi>
2019-11-02 21:35:25 +0200

    Added falling rocks

---
+++ Assets/Scripts/Falling_rock.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Falling_rock : MonoBehaviour
+{
+    private player_collider script;
+    public GameObject player;
+    public Rigidbody2D rb;
+    private Vector3 ogPos;
+    private Quaternion ogRot;
+
+    void Start()
+    {
+        rb.bodyType = RigidbodyType2D.Static;
+        ogPos = this.transform.position;
+        ogRot = this.transform.rotation;
+    }
+
+    private void Awake()
+    {
+
+        script = player.GetComponentInChildren<player_collider>();
+    
+    }
+
+    private void Update()
+    {
+        if (!script.is_player_alive) {
+
+            reset_rock();
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag == "Player") {
+            rb.bodyType = RigidbodyType2D.Dynamic;
+            this.tag = "Falling";
+        }
+        
+    }
+
+    private void reset_rock() {
+
+        rb.bodyType = RigidbodyType2D.Static;
+        transform.position = ogPos;
+        transform.rotation = ogRot;
+
+    }
+
+}
--- Assets/Scripts/player_collider.cs
+++ Assets/Scripts/player_collider.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class player_collider : MonoBehaviour
 {
 
+    public bool is_player_alive = true;
+
     public Vector3 cp_pos;
     public GameObject player;
     public GameObject broken;
@@ -31,43 +33,37 @@ public class player_collider : MonoBehaviour
         rb = player.GetComponent<Rigidbody2D>();
     }
 
+  
+
     private void OnCollisionEnter2D(Collision2D col)
     {
-
+        
         float force = GetImpactForce(col); 
-
         if ( force >= 100f) {
-
             hp -= force * 0.1f;
         }
-        
-        //print(col.impulse.magnitude);
-
-        /*
-        if (rb.velocity.magnitude > 1) {
-
-            print("sattui " + rb.velocity.magnitude);
-            hp -= rb.velocity.magnitude*4;
-           
-        }
-        */
-
 
         if (col.collider.tag == "cp") {
             cp_pos = col.collider.transform.position;
             }
 
         if (col.collider.tag == "bad") {
-
             Kill_Player();
         }
+
+        if (col.collider.tag == "Falling") {
+
+            hp -= col.otherCollider.attachedRigidbody.velocity.magnitude * 5f;
+            
+        };
 
     }
 
     public void Kill_Player() {
 
-        if (player.active) { 
+        if (player.active) {
 
+            is_player_alive = false;
             GameObject obj = (GameObject)Instantiate(broken, transform.position, transform.rotation);
             parts = obj.GetComponentsInChildren<Rigidbody2D>();
 
